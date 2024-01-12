4942341ba05541ba32c0848fc961ee8cb4b637a0
dw-dev <dimaswahyu.official@gmail.com>
2024-01-12 14:00:10 +0700

    handler button

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost({ onBodyChange, onAuthorChange, onCancel }) {
+function NewPost({ onCancel }) {
+  const [stateData, setStateData] = useState("");
+  const [stateAuthor, setStateAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setStateData(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setStateAuthor(event.target.value);
+  }
+
+  function onSubmitHandler(event){
+    event.preventDefault();
+    const postData ={
+      body: stateData,
+      author:stateAuthor
+    }
+    onCancel() 
+    console.log(postData)
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} />
+        <input type="text" id="name" required onChange={changeAuthorHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,37 +5,18 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 function PostList({isPosting , onStopPosting}) {
-  const [stateData, setStateData] = useState("");
-  const [stateAuthor, setStateAuthor] = useState("");
 
-  function changeBodyHandler(event) {
-    setStateData(event.target.value);
-  }
-
-  function changeAuthorHandler(event) {
-    setStateAuthor(event.target.value);
-  }
 
   return (
     <>
-    {/* {stateModal ? <Modal onClose={hideModalHandler}>
-        <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={changeAuthorHandler}
-        />
-      </Modal> : false} */}
       {isPosting && (<Modal onClose={onStopPosting}>
         <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={changeAuthorHandler}
           onCancel={onStopPosting}
         />
       </Modal>)}
       
       <ul className={list.posts}>
         <Post author="dimas" body="react is awesome" />
-        <Post author="wahyu" body="learning by doing is more efficient" />
-        <Post author={stateAuthor} body={stateData} />
       </ul>
     </>
   );
