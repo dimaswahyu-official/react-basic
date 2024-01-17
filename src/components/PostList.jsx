df83512a05dfe106ef4a7650f72a0652a4409824
dw-dev <dimaswahyu.official@gmail.com>
2024-01-17 16:24:41 +0700

    add fetch data from BE

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,18 +1,37 @@
 import list from "./PostList.module.css";
 import Post from "./Post";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
   const [post, setPost] = useState([]);
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchPost(){
+      setLoading(true)
+      const response = await fetch('http://localhost:8080/posts')
+      const resData = await response.json();
+      setPost(resData.posts)
+      setLoading(false)
+    }
+    fetchPost();
+  },[]);
 
   function addPostHandler(e) {
+    fetch('http://localhost:8080/posts' , {
+      method:'POST',
+      body:JSON.stringify(e),
+      headers:{
+        "Content-Type": "application/json"
+      }
+    })
     setPost((existingPost) => [e, ...existingPost]);
   }
   return (
     <>
-      {isPosting && (
+      {!isLoading && isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost onCancel={onStopPosting} onPosting={addPostHandler} />
         </Modal>
@@ -26,13 +45,15 @@ function PostList({ isPosting, onStopPosting }) {
         </ul>
       )}
 
-      {post.length === 0 && (
+      {!isLoading && post.length === 0 && (
         <div style={{textAlign:"center", color:"white"}}>
           <h1>There are no post yet</h1>
           <p>Start adding one!</p>
         </div>
         
       )}
+
+      {isLoading && <div style={{textAlign:"center", color:"white"}}><p>Loading the Data ...</p></div>}
     </>
   );
 }
